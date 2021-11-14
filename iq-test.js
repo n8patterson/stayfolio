e124e884823e99684983c678f1302486e95ac03c
Nathan Patterson <nathan.e.patterson@gmail.com>
2021-11-14 13:06:57 +0900

    iq test

---
+++ iq-test.js
@@ -0,0 +1,9 @@
+const iqTest = (array) => {
+  let arr = array.split(' ').map(Number);
+  let odd = arr.filter(num => num % 2 !== 0);
+  let even = arr.filter(num => num % 2 === 0);
+  return odd.length < even.length ? arr.indexOf(odd[0]) + 1 : arr.indexOf(even[0]) + 1;
+}
+
+console.log(iqTest("2 4 7 8 10")); //3 # Only 7 is an odd number
+console.log(iqTest("1 2 1 1")); //2 # Only 2 is an even number
